de89ab7b2f2070544434ca179b9001f98cd7ff12
Codey Sun <codey.sun@gmail.com>
2024-01-25 23:18:58 -0600

    updates

--- lua/codeyman2/plugins-setup.lua
+++ lua/codeyman2/plugins-setup.lua
@@ -217,15 +217,7 @@ return packer.startup(function(use)
 	use({
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			vim.opt.list = true
-			-- vim.opt.listchars:append("space:⋅")
-			-- vim.opt.listchars:append("eol:↴")
-
-			require("indent_blankline").setup({
-				space_char_blankline = " ",
-				show_current_context = true,
-				show_current_context_start = true,
-			})
+			require("ibl").setup({})
 		end,
 	})
 
